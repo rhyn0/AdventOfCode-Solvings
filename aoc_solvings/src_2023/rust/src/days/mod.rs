31edaa72fd167dfb1b0cf7993122e632071a7823
Ryan Ozawa <ryan.ozawa21@gmail.com>
2023-12-05 00:46:34 -0800

    do day 4

--- aoc_solvings/src_2023/rust/src/days/mod.rs
+++ aoc_solvings/src_2023/rust/src/days/mod.rs
@@ -1,6 +1,7 @@
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 pub mod prelude {
-    pub use super::{day01, day02, day03};
+    pub use super::{day01, day02, day03, day04};
 }
--- aoc_solvings/src_2023/rust/src/main.rs
+++ aoc_solvings/src_2023/rust/src/main.rs
@@ -14,7 +14,7 @@ fn solve_day(day: u16) -> fn(Option<&str>) -> SolutionPair {
         1 => Days::day01::solve,
         2 => Days::day02::solve,
         3 => Days::day03::solve,
-        // 4 => Days::day04::solve,
+        4 => Days::day04::solve,
         // 5 => Days::day05::solve,
         // 6 => Days::day06::solve,
         // 7 => Days::day07::solve,
