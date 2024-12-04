3cfcb8de45dcbd7416072571d8ce5b5f227ae247
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-12-03 22:19:21 -0800

    add: day 3 solved

---
+++ aoc_solvings/src_2024/cpp/src/solutions/day03.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+namespace aoc {
+void solve_day03_part1(const std::string &input_path);
+void solve_day03_part2(const std::string &input_path);
+} // namespace aoc
