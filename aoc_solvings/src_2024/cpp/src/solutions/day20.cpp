1aa8dc391d1f57d4d49e4e70f036524c28a5abab
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-12-21 11:48:35 -0800

    passing day 20 part 1, pre refactor

---
+++ aoc_solvings/src_2024/cpp/src/solutions/day20.cpp
@@ -0,0 +1,229 @@
+#include "day20.hpp"
+#include "utils/input_reader.hpp"
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#define CHEAT_THRESHOLD 100
+
+typedef std::pair<int, int> POS;
+template <> struct std::hash<std::pair<int, int>> {
+  size_t operator()(const std::pair<int, int> &p) const {
+    return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+  }
+};
+template <> struct std::hash<std::pair<POS, POS>> {
+  size_t operator()(const std::pair<POS, POS> &p) const {
+    return hash<POS>()(p.first) ^ (hash<POS>()(p.second) << 1);
+  }
+};
+namespace day20 {
+std::vector<POS> neighbors(POS pos) {
+  std::vector<POS> neighbors;
+  neighbors.push_back(std::make_pair(pos.first - 1, pos.second));
+  neighbors.push_back(std::make_pair(pos.first + 1, pos.second));
+  neighbors.push_back(std::make_pair(pos.first, pos.second - 1));
+  neighbors.push_back(std::make_pair(pos.first, pos.second + 1));
+  return neighbors;
+}
+enum class RaceCell {
+  EMPTY,
+  WALL,
+};
+class RaceGrid {
+public:
+  std::vector<std::vector<RaceCell>> grid;
+  POS start, end;
+  RaceGrid(const std::string &filepath) {
+    std::string content = FileReader::readFile(filepath);
+    grid = {{}};
+    for (char c : content) {
+      if (c == '\n') {
+        grid.push_back(std::vector<RaceCell>());
+      } else {
+        if (c == 'S') {
+          start = std::make_pair(grid.size() - 1, grid[grid.size() - 1].size());
+        } else if (c == 'E') {
+          end = std::make_pair(grid.size() - 1, grid[grid.size() - 1].size());
+        }
+        grid[grid.size() - 1].push_back(c == '#' ? RaceCell::WALL
+                                                 : RaceCell::EMPTY);
+      }
+    }
+    if (grid[grid.size() - 1].empty()) {
+      grid.pop_back();
+    }
+  }
+  bool on_board(POS pos) {
+    return pos.first >= 0 && pos.first < static_cast<int>(grid.size()) &&
+           pos.second >= 0 && pos.second < static_cast<int>(grid[0].size());
+  }
+  bool can_move_to(POS pos) {
+    return on_board(pos) && grid[pos.first][pos.second] == RaceCell::EMPTY;
+  }
+  void print_visited(std::unordered_set<POS> &visited) {
+    for (size_t row = 0; row < grid.size(); row++) {
+      for (size_t col = 0; col < grid[row].size(); col++) {
+        if (visited.contains(std::make_pair(row, col))) {
+          std::cout << "O";
+        } else if (grid[row][col] == RaceCell::WALL) {
+          std::cout << "#";
+        } else {
+          std::cout << ".";
+        }
+      }
+      std::cout << "\n";
+    }
+  }
+  std::vector<POS> find_path_length(POS curr_pos) {
+    std::deque<std::pair<POS, std::vector<POS>>> queue = {
+        std::make_pair(curr_pos, std::vector<POS>())};
+    std::unordered_set<POS> visited;
+    visited.insert(curr_pos); // Mark starting position as visited
+
+    while (!queue.empty()) {
+      std::pair<POS, std::vector<POS>> curr = queue.front();
+      queue.pop_front();
+
+      if (curr.first == this->end) {
+        return curr.second;
+      }
+
+      for (POS neighbor : neighbors(curr.first)) {
+        if (can_move_to(neighbor) && visited.find(neighbor) == visited.end()) {
+          visited.insert(neighbor); // Mark as visited when adding to queue
+          // copy the vector and add the neighbor
+          std::vector<POS> new_path = curr.second;
+          new_path.push_back(neighbor);
+          queue.push_back(std::make_pair(neighbor, new_path));
+        }
+      }
+    }
+    return std::vector<POS>();
+  }
+  RaceCell get_cell(POS pos) { return grid[pos.first][pos.second]; }
+  std::vector<POS> find_cheat_from_point(const std::vector<POS> &path,
+                                         POS point, int max_cheat_length = 2) {
+    std::vector<POS> ret;
+    std::deque<std::pair<POS, int>> queue = {std::make_pair(point, 0)};
+    while (!queue.empty()) {
+      std::pair<POS, int> curr = queue.front();
+      queue.pop_front();
+      // if the length of the cheat has reached the max, we can stop with this
+      // path
+      if (curr.second == max_cheat_length) {
+        continue;
+      }
+      for (auto neighbor : neighbors(curr.first)) {
+        if (!on_board(neighbor) || neighbor == point) {
+          continue;
+        }
+        queue.push_back(std::make_pair(neighbor, curr.second + 1));
+        if (std::find(path.begin(), path.end(), neighbor) != path.end()) {
+          ret.push_back(neighbor);
+        }
+      }
+    }
+    // for (auto neighbor : neighbors(point)) {
+    //   if (!on_board(neighbor) ||
+    //       grid[neighbor.first][neighbor.second] != RaceCell::WALL) {
+    //     continue;
+    //   }
+    //   for (auto pos_point_on_path : neighbors(neighbor)) {
+    //     if (!on_board(pos_point_on_path) || pos_point_on_path == point) {
+    //       continue;
+    //     }
+    //     if (std::find(path.begin(), path.end(), pos_point_on_path) !=
+    //         path.end()) {
+    //       ret.push_back(pos_point_on_path);
+    //     }
+    //   }
+    // }
+    return ret;
+  }
+};
+int calculate_cheat_distance(size_t start_idx, size_t end_idx) {
+  return end_idx - start_idx - 2;
+}
+}; // namespace day20
+inline std::ostream &operator<<(std::ostream &os, const day20::RaceGrid &grid) {
+  for (size_t row = 0; row < grid.grid.size(); row++) {
+    for (size_t col = 0; col < grid.grid[row].size(); col++) {
+      switch (grid.grid[row][col]) {
+      case day20::RaceCell::EMPTY:
+        os << ".";
+        break;
+      case day20::RaceCell::WALL:
+        os << "#";
+        break;
+      }
+    }
+    os << "\n";
+  }
+  return os;
+}
+namespace aoc {
+using namespace day20;
+
+void solve_day20_part1(const std::string &input_path) {
+  RaceGrid grid(input_path);
+  std::vector<POS> path = grid.find_path_length(grid.start);
+  std::unordered_map<std::pair<POS, POS>, int> cheat_map;
+  int above_threshold_cheats = 0;
+  // path doesn't include the start point, which is a valid cheating spot
+  for (auto cheat_end : grid.find_cheat_from_point(path, grid.start)) {
+    auto map_key = std::make_pair(grid.start, cheat_end);
+    // note the negative one here, because the start is BEFORE the taken path
+    int cheat_distance_savings = calculate_cheat_distance(
+        -1, std::find(path.begin(), path.end(), cheat_end) - path.begin());
+    if (cheat_distance_savings <= 0) {
+      // no savings in this case
+      continue;
+    }
+    if (!cheat_map.contains(map_key)) {
+      // if the savings are better than the previous one, replace it
+      cheat_map[map_key] = cheat_distance_savings;
+    } else if (cheat_map[map_key] < cheat_distance_savings) {
+      cheat_map[map_key] = cheat_distance_savings;
+    }
+  }
+  for (POS point : path) {
+    int starting_idx_on_path =
+        std::find(path.begin(), path.end(), point) - path.begin();
+    for (auto cheat_end : grid.find_cheat_from_point(path, point)) {
+      int second =
+          std::find(path.begin(), path.end(), cheat_end) - path.begin();
+      int cheat_distance_savings =
+          calculate_cheat_distance(starting_idx_on_path, second);
+      auto map_key = std::make_pair(point, cheat_end);
+      if (cheat_distance_savings <= 0) {
+        // no savings in this case
+        continue;
+      }
+      if (cheat_map.find(map_key) == cheat_map.end()) {
+        // insert because it doesn't exist
+        cheat_map[map_key] = cheat_distance_savings;
+      } else {
+        // if the savings are better than the previous one, replace it
+        if (cheat_map[map_key] < cheat_distance_savings) {
+          cheat_map[map_key] = cheat_distance_savings;
+        }
+      }
+    }
+  }
+  for (auto [key, value] : cheat_map) {
+    if (value >= CHEAT_THRESHOLD) {
+      above_threshold_cheats++;
+    }
+  }
+  std::cout << "\nPart 1: " << above_threshold_cheats << std::endl;
+}
+void solve_day20_part2(const std::string &input_path) {
+  std::cout << "\nPart 2: " << input_path << std::endl;
+}
+}; // namespace aoc
